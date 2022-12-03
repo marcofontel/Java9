64e258576edaa140fd83c0ba6f0298a72324e9c2
server <wll.rgr@gmail.com>
2022-12-03 20:07:35 -0300

    Implementa Interface com metodos privados

--- src/org/arca/intmetpriva/IntMetodoPriv.java
+++ src/org/arca/intmetpriva/IntMetodoPriv.java
@@ -1,5 +1,25 @@
 package org.arca.intmetpriva;
 
-public class IntMetodoPriv {
+public interface IntMetodoPriv {
 
+	default void logInfo(String mensagem) {
+		log(mensagem, "INFO");
+	}
+	
+	default void logWarn(String mensagem) {
+		log(mensagem, "WARN");
+	}
+	
+	default void logError(String mensagem) {
+		log(mensagem, "ERROR");
+	}
+	
+	private void log(String mensagem, String tipo) {
+		System.out.println("Inicio da Mesagem");
+		
+		System.out.println("Tipo: " + tipo+": " + mensagem );
+		
+		System.out.println("Fim da Mensagem");
+	}
+	
 }
---
+++ src/org/arca/intmetpriva/TestEventos.java
@@ -0,0 +1,11 @@
+package org.arca.intmetpriva;
+
+public class TestEventos {
+
+	public static void main(String[] args) {
+		IntMetodoPriv log = new IntMetodoPriv() {
+		};
+		log.logError("Falhou");
+		
+	}
+}
