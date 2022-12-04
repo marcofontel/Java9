a6233f5c5ffd2e9ebbe70669f853e86701841bd7
server <wll.rgr@gmail.com>
2022-12-04 05:59:08 -0300

    Exemplo padrão

--- src/org/arca/apistream/TesteApiStream.java
+++ src/org/arca/apistream/TesteApiStream.java
@@ -1,8 +1,23 @@
 package org.arca.apistream;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TesteApiStream {
 
+	
 	public static void main(String[] args) {
+		List<String> lista = new ArrayList<>();
+		lista.add("Geladeira");
+		lista.add("Fogao");
+		lista.add("Tv Samsung");
+		lista.add("Armario");
+		lista.add("Cadeira");
+		lista.add("Janela");
 		
+		lista.stream()
+		.filter(a->a.toString().contains("Geladeira"))
+		.map(a -> a.toString())
+		.forEach(a -> System.out.println(a));
 	}
 }
