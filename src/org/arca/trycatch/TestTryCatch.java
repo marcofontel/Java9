123f2af3ecc315a7bf95b509959806953a2a8397
server <wll.rgr@gmail.com>
2022-12-04 04:55:25 -0300

    Clausula Finaly

--- src/org/arca/trycatch/TestTryCatch.java
+++ src/org/arca/trycatch/TestTryCatch.java
@@ -1,5 +1,30 @@
 package org.arca.trycatch;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TestTryCatch {
 
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void testTryCatch(String path) throws IOException {
+		
+		String linha="";
+		BufferedReader bf = new BufferedReader(new FileReader(path));
+		try {
+			while((linha = bf.readLine()) != null) {
+				System.out.println(linha);
+			}
+		} catch (IOException e) {
+			throw e;
+		}
+		finally {
+			if(bf != null) {
+				bf.close();
+			}
+		}
+	}
 }
