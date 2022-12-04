df09bf51a35d058194fe2354b1883519df9ac964
server <wll.rgr@gmail.com>
2022-12-04 05:06:43 -0300

    Limpando bloco antigo

--- src/org/arca/trycatch/TestTryCatch.java
+++ src/org/arca/trycatch/TestTryCatch.java
@@ -10,29 +10,12 @@ public class TestTryCatch {
 		lerArquivoTrycRecurso("exemplo.txt");
 	}
 	
-	public static void testTryCatch(String path) throws IOException {
-		
-		String linha="";
-		BufferedReader bf = new BufferedReader(new FileReader(path));
-		try {
-			while((linha = bf.readLine()) != null) {
-				System.out.println(linha);
-			}
-		} catch (IOException e) {
-			throw e;
-		}
-		finally {
-			if(bf != null) {
-				bf.close();
-			}
-		}
-	}
 	
 	public static void lerArquivoTrycRecurso(String file) throws IOException {
 		String linha="";
 		BufferedReader br = new BufferedReader(new FileReader(file));
+		
 		try(br){
-			
 			while((linha = br.readLine()) != null) {
 				System.out.println(linha);
 			}
