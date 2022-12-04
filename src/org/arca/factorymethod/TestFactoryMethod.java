870eb559f4ee0c861da647f81d18ccea37c04023
server <wll.rgr@gmail.com>
2022-12-04 05:31:46 -0300

    Iniciando exemplo padrão comparativo

--- src/org/arca/factorymethod/TestFactoryMethod.java
+++ src/org/arca/factorymethod/TestFactoryMethod.java
@@ -1,5 +1,15 @@
 package org.arca.factorymethod;
 
+import java.util.List;
+
 public class TestFactoryMethod {
 
+	public static void main(String[] args) {
+		List<String> lista = List.of("Banana, Maçã, Pera");
+		
+		for(String a: lista) {
+			System.out.println(a);
+		}
+		 
+	}
 }
