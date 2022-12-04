d5b47e411c14d175c2a6430913ced105fb54a3b4
server <wll.rgr@gmail.com>
2022-12-04 05:34:54 -0300

    Ajuste...

--- src/org/arca/factorymethod/TestFactoryMethod.java
+++ src/org/arca/factorymethod/TestFactoryMethod.java
@@ -1,11 +1,11 @@
 package org.arca.factorymethod;
 
-import java.util.List;
+import java.util.Set;
 
 public class TestFactoryMethod {
 
 	public static void main(String[] args) {
-		List<String> lista = List.of("Banana, Maçã, Pera");
+		Set<String> lista = Set.of("Banana", "Maçã", "Pera");
 		
 		for(String a: lista) {
 			System.out.println(a);
